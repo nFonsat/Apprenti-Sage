91d23bd2aa6d1bc355c611e4fe1a1e8ea539c3d2
Mary Aurelien <maryaurelien38@gmail.com>
2015-01-09 11:29:02 +0100

    Remove file

--- app/src/main/java/com/lpiem/apprentisage/ihm/AccueilActivity.java
+++ app/src/main/java/com/lpiem/apprentisage/ihm/AccueilActivity.java
@@ -15,7 +15,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
