ca62bbf5311e0bb6f411abda47b9f89033b9d515
Mary Aurelien <maryaurelien38@gmail.com>
2015-01-14 11:44:08 +0100

    bouton retour sousActivite

--- app/src/main/java/com/lpiem/apprentisage/ihm/SousCategorieActivity.java
+++ app/src/main/java/com/lpiem/apprentisage/ihm/SousCategorieActivity.java
@@ -4,7 +4,7 @@
 *
 * SousCategorieActivity.java
 * 
-* Michael Breton - Clément Bretin
+* Michael Breton - Clï¿½ment Bretin
 * LP IEM - 2014
 *
 *******************************************************************************************************************/
@@ -12,6 +12,8 @@
 package com.lpiem.apprentisage.ihm;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockActivity;
@@ -22,15 +24,25 @@ import com.lpiem.apprentisage.adapter.SubCatAdapter;
 public class SousCategorieActivity extends SherlockActivity
 {
 	private ListView listCategorie;
+    private Button btnRetour;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sub_categorie_list);
+
+        btnRetour = (Button)findViewById(R.id.retour);
 		
 		listCategorie = (ListView) findViewById(R.id.sub_categorie_list);
 		listCategorie.setAdapter(new SubCatAdapter(this));
+
+        btnRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 		
 		ActionBarService.initActionBar(this, getSupportActionBar(), getString(R.string.cat_titre));
 	}
